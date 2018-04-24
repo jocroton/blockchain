4ff08c411bebb448c00085d6e9571a59950576b3
jocroton <38695013+jocroton@users.noreply.github.com>
2018-04-24 16:04:16 +0200

    Add files via upload

---
+++ code.py
@@ -0,0 +1,56 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Apr 24 14:07:57 2018
+
+@author: johan
+"""
+from __future__ import division
+"import random, time, toolkit
+"import powerlaw as pl
+import networkx as nx
+import numpy as np
+import math as m
+import matplotlib.pyplot as plt
+
+num_nodes = 100        # number of nodes in the network
+nodes_conn = 8      
+
+"initialize network
+naka_net = nx.erdos_renyi_graph(num_nodes, float(nodes_conn) / float(num_nodes))
+
+"create list of neighbors for each node
+neighbor_list=list()
+for line in nx.generate_adjlist(naka_net): 
+    print(line)
+    line=[int(s) for s in line.split(' ')]
+    neighbor_list.append(line)
+   
+"matrix of nodes and information
+info_list=np.zeros((100,3))
+
+"first column: node IDs
+info_list[:,0]=list(range(100))
+
+"third column: block IDs, set all to 1
+info_list[:,2]=1
+    
+"pseudo code for gossip
+increment=[] 
+
+def gossip():
+"find nodes in state=1
+"find neighbors for each node in state 1
+for nodes[status]==1:
+    if increment[node] < range_neighbors:
+    "pick random neighbor
+    "remove neighbor from neighbor_list
+        if block[node]>block[neighbor]:
+            "update neighbor block
+            "neighbor block length +1
+            "reset neighbors neighbor_list 
+            "reset increment=0
+    else node[]=0
+  
+    
+
+    
