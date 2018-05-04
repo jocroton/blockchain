566a58c0f608edd0e2eccb904c82f6f3a93335c1
NikoBauer <38761343+NikoBauer@users.noreply.github.com>
2018-05-04 12:15:03 +0200

    Netzwerkinitialisierung
    isst zwar sicher etwas umständlich, aber wenigstens gibt es das ergebnis. Wie das mit dem Networx package funktioniert hab ich nicht ganz verstanden...

--- code.py
+++ code.py
@@ -17,6 +17,7 @@ random.seed(100)
 num_nodes = 100        # number of nodes in the network
 nodes_conn = 8      
 
+"""
 #initialize network
 naka_net = nx.erdos_renyi_graph(num_nodes, float(nodes_conn) / float(num_nodes))
 
@@ -29,7 +30,45 @@ for line in nx.generate_adjlist(naka_net):
     
 for i in range(len(neighbor_list)):  #remove the node ID from the neighbor-list
     del neighbor_list[i][0]
-        
+ """
+
+def random_graph(n, k):
+    # A function that creates a random graph, by Martin Ritchie
+    # 08/06/2016. This funciton is a revision of my first 
+    # attempt in python to code the Erdos-Renyi random graph
+    # algortihtm. The code now include an adjacency list and
+    # edge list as ouput in addtion to the adjacency matrix. 
+    # ------------------- Inputs --------------------------
+    # N: number of nodes.
+    # k: desired average degree.
+    # ------------------- Outputs -------------------------
+    # A: adjacency matrix (line 16 for sparse).
+    # A_list: adjacency list.
+    # edge_list: list of edges. 
+   # import ipdb
+    # ipdb.set_trace()
+    # A = lil_matrix((n, n), dtype=np.int8)
+    A_list = [[] for i in range(n)] 
+    edge_list = []
+    p = 1 - (float(k) / (n - 1))
+    A = np.random.random((n, n))
+    A *= np.tri(*A.shape)
+    np.fill_diagonal(A, 0)
+    
+    edge = A>p 
+    A[edge] = 1
+    no_edge = A <= p 
+    A[no_edge] = 0  # All low values set to 0
+    A = np.add(A, A.transpose())
+
+    for i in range(n):
+        for j in range(i-1):
+            if A[i, j] > 0: 
+                A_list[i] = A_list[i] + [j]
+                A_list[j] = A_list[j] + [i]
+                edge_list.append((i,j))
+                edge_list.append((j,i))
+    return (A, A_list, edge_list) 
   
 def gossiping():
      #iterate throuhg all gossiping nodes
