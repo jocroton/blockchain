aaee1e3e0001e9dc78855c368d1b6ad5aefeffb4
NikoBauer <38761343+NikoBauer@users.noreply.github.com>
2018-05-08 13:42:50 +0200

    working

--- code.py
+++ code.py
@@ -7,10 +7,11 @@ Created on Tue Apr 24 14:07:57 2018
 from __future__ import division
 import random
 #import powerlaw as pl
-import networkx as nx
+#import networkx as nx
 import numpy as np
-import math as m
-import matplotlib.pyplot as plt
+#import math as m
+#import matplotlib.pyplot as plt
+import copy
 
 
 random.seed(100)
@@ -97,59 +98,71 @@ def delay_time(L, n):
     """
     
     
-    epsilon = np.random.poisson(L, n)
+    epsilon = np.random.poisson(L, n+1)
     
     delay=list()
     delay.append(epsilon[0])
+
     
     for e in range(n-1):
 
-        delay.append(delay[e] + epsilon[e+1])
+        delay.append(delay[e] + epsilon[e + 1])
     return(delay)
+    
+    
+
         
 def new_block(lucky, num):  #creates a new block 
     """
     """
+    global last_block
+    global info_list
+    global neighbors
+    global delay_list
     
-    parent_block = last_block[lucky]   
-    block_ID = ([parent_block, num])
+    block_ID = copy.copy(last_block[lucky])  
     block_ID.append(num)   #current block number added to parent block: new ID
-    last_block[lucky] = block_ID #add new block to lucky's chain
+    print(block_ID)
+    last_block[lucky] = copy.copy(block_ID) #add new block to lucky's chain
     
     info_list[lucky, 1] = 1
-    neighbors[lucky] = neighbor_list[lucky] #start gossiping to all neighbors again
+    neighbors[lucky] = copy.copy(neighbor_list[lucky]) #start gossiping to all neighbors again
   #need to update the listeners delay times (newly started gossiping)
     delay_list[lucky] = delay_time(network_delay, len(neighbor_list[lucky]))
     delay_list[lucky] = [x+t for x in delay_list[lucky]]
 
 
 def gossiping():
-    
+    global delay_list
+    global neighbor_list
+    global neighbors
+    global last_block
+    global info_list
+    global last_block
      #iterate throuhg all gossiping nodes
-        for gossiper in gossipers:
-            
+    for gossiper in gossipers:
 
-             listeners = neighbors[gossiper] #all the neighbors the active node has not yet gossiped to
+         listeners = neighbors[gossiper] #all the neighbors the active node has not yet gossiped to
+         
+         if len(listeners) == 0 :    #if there are no more neighbors to gossip to
+             info_list[gossiper,1] = 0
              
-             if len(listeners) == 0 :    #if there are no more neighbors to gossip to
-                 info_list[gossiper,1] = 0
+         elif delay_list[gossiper][0] < t :  #time is > than the delay for the next gossip round
+            
+             choose = random.randrange(0,len(listeners)) #choose a random node to gossip to
+             listener = listeners[choose]
+             del neighbors[gossiper][choose] #remove the neighbor
+             del delay_list[gossiper][0] #next entry will be the time it takes to gossip to two nodes
+             
+             if len(last_block[gossiper]) > len(last_block[listener]) : #compare chains (block-ID)
                  
-             elif delay_list[gossiper][0] < t :  #time is > than the delay for the next gossip round
-                
-                 choose = random.randrange(0,len(listeners)) #choose a random node to gossip to
-                 listener = listeners[choose]
-                 del neighbors[gossiper][choose] #remove the neighbor
-                 del delay_list[gossiper][0] #next entry will be the time it takes to gossip to two nodes
-                 
-                 if len(last_block[gossiper]) > len(last_block[listener]) : #compare chains (block-ID)
-                     
-                     last_block[listener] = last_block[gossiper] #adopt gossipers Chain
-                     info_list[listener][1] = 1 #change the state
-                     neighbors[listener] = neighbor_list[listener] #start gossiping to all neighbors again
-                      #need to update the listeners delay times (newly started gossiping)
-                     delay_list[listener] = delay_time(network_delay, len(neighbor_list[listener]))
-                     delay_list[listener] = [x+t for x in delay_list[listener]]
-       
+                 last_block[listener] = copy.copy(last_block[gossiper]) #adopt gossipers Chain
+                 info_list[listener][1] = 1 #change the state
+                 neighbors[listener] = copy.copy(neighbor_list[listener]) #start gossiping to all neighbors again
+                  #need to update the listeners delay times (newly started gossiping)
+                 delay_list[listener] = delay_time(network_delay, len(neighbor_list[listener]))
+                 delay_list[listener] = [x+t for x in delay_list[listener]]
+   
                         
 #initialize the network
 network = random_graph(num_nodes, nodes_conn)
@@ -177,8 +190,8 @@ last_block[new] = [0,1]  # record block ID
 comp_power = np.random.exponential(expo_scale, int(num_nodes*(1-dilusion_rate)))
 miners = random.sample(range(num_nodes), int(num_nodes*(1-dilusion_rate)))
 info_list[miners,2] = comp_power
-probability_dt = info_list[:,2]/sum(info_list[:,2])
-probability = probability_dt  #variable that will be changed below
+probability_dt = (info_list[:,2]/sum(info_list[:,2]))/100
+probability = copy.copy(probability_dt)  #variable that will be changed below
 
   
     
@@ -191,8 +204,15 @@ for i in range(num_nodes):
 
 gossipers = info_list[:,0][info_list[:,1]==1] #nodes in state one
 gossipers = [int(i) for i in gossipers]
-neighbors = neighbor_list #we will delete and add the neighbors from this list
+
+neighbors = copy.copy(neighbor_list) #we will delete and add the neighbors from this list
 #neighbors = [neighbor_list[i] for i in gossipers] #neighbors to those nodes in state one
+
+neighbors[0]
+neighbor_list[0]
+del neighbors[0][0]
+neighbor_list[0]
+
 t=0 
  
 gossiping_round = 0
@@ -203,27 +223,28 @@ gossiping_round = 0
 
     
         
-while len(gossipers) > 0 :
+while t <10000 :
 
     gossiping() #one round of gossip, all nodes in state one talk to one of their neighbors
     
     gossiping_round = gossiping_round + 1
-    print("gossiping round number:")
-    print(gossiping_round)
+    #print("gossiping round number:")
+    #print(gossiping_round)
     # block creation
     
-    """
+
     for i in range(num_nodes):  #go through all the nodes
         lottery = random.uniform(0.0, 1.0)
         if lottery > probability[i]:    #did the node mine a new block?
             probability[i] = (1-probability[i]) * probability[i]   
         else:
-            new_block(i, block_num)
             block_num = block_num + 1
+            new_block(i, block_num)
             probability[i] = probability_dt[i]
-            print("new block mined")
+            print("new block mined by")
+            print(i)
            
-       """  
+       
                 
     t=t+1
     gossipers = info_list[:,0][info_list[:,1]==1] #nodes in state one
@@ -246,4 +267,3 @@ while len(gossipers) > 0 :
  
        
 
-    
